ce6c86b4d954ee87deb4cfa6765e90dafd94c7e0
aras2855 <125977849+aras2855@users.noreply.github.com>
2023-02-22 11:16:19 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "MTA3Nzg1NDEyODY1MjQzNTUyOA.GBNeQh.wSZMCYtbveaigCOQnHbw_3Mz5UCrht-LVlzy_Q",
+    prefix: ".",
+    content: "bok",
+    channel: "1074464657563205645",
+    button: "1077863273430519839",
+    yetkili: "1074464869094522933",
+    log: "1077307483397312652"
+}
